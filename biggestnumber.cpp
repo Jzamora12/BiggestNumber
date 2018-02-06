1210dbb5611e84e7a7e0e8a1571f9e3774dec8e1
Jzamora12 <jzamora12@mail.csuchico.edu>
2018-02-06 10:56:20 -0800

    ask user for two different whole numbers and stores them.

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,15 +1,16 @@
-//Author:
+//Author: Joseluis Zamora 
 #include<iostream>
 
 using namespace std; //so we don't need the prefix std::
 
 int main()
 {
-  
+  int Number1;
+  int Number2;
   cout<<"Please enter a whole number:\n";
-  
+  std::cin >> Number1; 
   cout<<"Please enter another whole number:\n";
-
+  std::cin >> Number2;
   cout<<"Of those two numbers, the biggest is: ";
   cout<<endl<<"Thank you for playing.\n";
 
