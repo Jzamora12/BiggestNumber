3c60858f63b09742a3d0f561872e9438f466c662
Jzamora12 <jzamora12@mail.csuchico.edu>
2018-02-06 11:09:49 -0800

    Will now tell user which number is the biggest.

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -7,11 +7,15 @@ int main()
 {
   int Number1;
   int Number2;
+  int biggest;
   cout<<"Please enter a whole number:\n";
   std::cin >> Number1; 
   cout<<"Please enter another whole number:\n";
   std::cin >> Number2;
-  cout<<"Of those two numbers, the biggest is: ";
+  if( Number1 > Number2 )
+  { biggest=Number1;}
+  else { biggest=Number2;} 
+  cout<<"Of those two numbers, the biggest is: " << biggest;
   cout<<endl<<"Thank you for playing.\n";
 
   return 0;
